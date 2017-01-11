d54e3bdd11f4bba7237eae70ac271e95d67a3bfe
Brian Sayler <saylerb@users.noreply.github.com>
2017-01-10 21:29:43 -0700

    add inheritance solution (#1)

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,52 +4,8 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+  def update_quality!
+    @items.each(&:update_quality!)
   end
 end
 
@@ -62,7 +18,35 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+
+  def update_quality!
+    @quality = @quality - 1 if @quality > 0
+    @sell_in = @sell_in - 1
+    @quality = @quality - 1 if @sell_in < 0 && @quality > 0
+  end
+end
+
+class AgedBrie < Item
+  def update_quality!
+    @quality = @quality + 1 if @quality < 50
+    @sell_in = @sell_in - 1
+  end
+end
+
+class BackstagePass < Item
+  def update_quality!
+    if @sell_in > 10
+      @quality = @quality + 1
+    elsif @sell_in > 5
+      @quality = @quality + 2
+    elsif @sell_in > 0
+      @quality = @quality + 3
+    else
+      @quality = 0
+    end
+    @sell_in -= 1
   end
 end
